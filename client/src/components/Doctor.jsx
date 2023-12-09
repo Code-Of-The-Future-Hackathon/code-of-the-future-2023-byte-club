5b27822e213f059200af840346d719d8c641dad7
HHChandar20 <HHChandar20@codingburgas.bg>
2023-12-09 11:36:46 +0200

    Implement AI-Doctor functionality

--- client/src/components/Doctor.jsx
+++ client/src/components/Doctor.jsx
@@ -1,11 +1,118 @@
-import Navbar from '../components/Navbar';
+import React, { useState } from "react";
+import axios from "axios";
+import { Navbar } from "./";
 
 const Doctor = () => {
+  const [selectedSymptoms, setSelectedSymptoms] = useState([]);
+  const [response, setResponse] = useState("");
+
+  const handleSymptomSelect = (symptom) => {
+    if (!selectedSymptoms.includes(symptom)) {
+      setSelectedSymptoms([...selectedSymptoms, symptom]);
+    }
+  };
+
+  const handleSubmit = async () => {
+    try {
+      const prompt = `User selected symptoms: ${selectedSymptoms.join(", ")}\n\nDiagnose the possible sicknesses
+       and provide maximum 2 additional symptoms for each diagnosis
+       and recommend treatmant ways but if doctor help is needed recommend
+       doctor visiting a doctor. (You have 75 words and don't go for too
+       much in details and don't use too complex terminologies. 
+       The response should be in format: Possible sickness: {Sickness Name}:
+       \n Symptoms: {Symptoms of the sickness} \n Treatment: {Treatment in 10 
+        words maximum}). Add 3 newlines after treatment`;
+      
+      const serverResponse = await axios.post("http://localhost:8080/chat", {
+        userPrompt: prompt,
+      });
+  
+      setResponse(serverResponse.data.doctorResponse);
+    } catch (error) {
+      console.error("Error fetching response from server:", error);
+    }
+  };
+  
+  
+
   return (
     <>
-      <Navbar />
+      <Navbar/>
+
+      <div className="mt-20">
+        <h1>Doctor Chat</h1>
+
+        <div>
+          <h2>Select Symptoms:</h2>
+          <div className="flex flex-col">
+            <label>
+              <input
+                type="checkbox"
+                value="Fever"
+                onChange={() => handleSymptomSelect("Fever")}
+              />
+              Fever
+            </label>
+            <label>
+              <input
+                type="checkbox"
+                value="Headache"
+                onChange={() => handleSymptomSelect("Headache")}
+              />
+              Headache
+            </label>
+            <label>
+              <input
+                type="checkbox"
+                value="Temperature"
+                onChange={() => handleSymptomSelect("Temperature")}
+              />
+              Temperature
+            </label>
+            <label>
+              <input
+                type="checkbox"
+                value="Stomach Cramps"
+                onChange={() => handleSymptomSelect("Stomach Cramps")}
+              />
+              Stomach Cramps
+            </label>
+            <label>
+              <input
+                type="checkbox"
+                value="Leg pain"
+                onChange={() => handleSymptomSelect("Leg pain")}
+              />
+              Leg pain
+            </label>
+            <label>
+              <input
+                type="checkbox"
+                value="Muscle aches"
+                onChange={() => handleSymptomSelect("Muscle aches")}
+              />
+              Muscle aches
+            </label>
+          </div>
+          {/* Add more symptoms*/}
+        </div>
+      
+        <button onClick={handleSubmit} className="w-1/6 bg-indigo-100">Get Diagnosis</button>
+
+        {response && (
+          <div>
+            <h2>Doctor's Response:</h2>
+            <p
+              dangerouslySetInnerHTML={{
+                __html: response.replace(/\\n/g, "<br />"),
+              }}
+            />
+          </div>
+        )}
+
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default Doctor;
--- server/server.mjs
+++ server/server.mjs
@@ -1,12 +1,30 @@
 import express from "express";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
 import cors from "cors";
+import OpenAI from "openai";
+
 
 const app = express();
-dotenv.config();
+app.use(express.json());
 
 app.use(cors());
+
+const openai = new OpenAI({
+    apiKey: "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"
+})
+
+app.post('/chat', async (req, res) => {
+    const userPrompt = req.body.userPrompt;
+    console.log(userPrompt);
+  
+    const response = await openai.chat.completions.create({
+      model: 'gpt-3.5-turbo',
+      messages: [{ "role": "user", "content": userPrompt }],
+      max_tokens: 120,
+    });
+    console.log(response.choices[0].message.content);
+    res.json({ doctorResponse: response.choices[0].message.content.replace(/\n/g, "\\n") });
+  });
+  
 
 // Define a route
 app.get("/", (req, res) => {
@@ -16,15 +34,14 @@ app.get("/", (req, res) => {
     });
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = 8080;
 
-mongoose
-    .connect(process.env.CONNECTION_URL)
-    .then(() => {
-        app.listen(PORT, () => {
-            console.log(`Server is running on port: ${PORT}`);
-        })
+try
+{
+    app.listen(PORT, () =>
+    {
+        console.log(`Server is running on port: ${PORT}`);
     })
-    .catch((error) => {
-        console.error(error.message);
-    });
+} catch(error) {
+    console.error(error.message)
+}
