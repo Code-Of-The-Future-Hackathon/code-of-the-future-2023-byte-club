bfd90f6db1ec07df8ed213fd0b20bd407015eb49
HHChandar20 <HHChandar20@codingburgas.bg>
2023-12-10 01:25:04 +0200

    Add example AI Doctor response

--- server/server.mjs
+++ server/server.mjs
@@ -1,28 +1,47 @@
 import express from "express";
 import cors from "cors";
-import OpenAI from "openai";
-
+//import OpenAI from "openai";
 
 const app = express();
 app.use(express.json());
 
 app.use(cors());
 
-const openai = new OpenAI({
-    apiKey: "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"
-})
+const exampleResponse = {
+    "id": "chatcmpl-xyz123",
+    "object": "chat.completion",
+    "created": 1677649420,
+    "model": "gpt-3.5-turbo",
+    "usage": { "prompt_tokens": 30, "completion_tokens": 31, "total_tokens": 61 },
+    "choices": [
+      {
+        "message": {
+          "role": "assistant",
+          "content": "Possible sickness 1: Muscle Strain:\nSymptoms: Leg pain, Muscle Aches, Limited range of motion\nTreatment: Rest, Ice, Compression, Elevation (RICE), Over-the-counter pain relievers\nSpeciality: Orthopedist\nPossible sickness 2: Peripheral Arterial Disease (PAD):\nSymptoms: Leg pain, Muscle Aches, Weak or absent pulses\nTreatment: Lifestyle changes, Medications, Exercise\nSpeciality: Vascular specialist"
+        },
+        "finish_reason": "stop",
+        "index": 0
+      }
+    ]
+  };
+  
+  
+
+//const openai = new OpenAI({
+//    apiKey: "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"
+//})
 
 app.post('/chat', async (req, res) => {
     const userPrompt = req.body.userPrompt;
     console.log(userPrompt);
   
-    const response = await openai.chat.completions.create({
-      model: 'gpt-3.5-turbo',
-      messages: [{ "role": "user", "content": userPrompt }],
-      max_tokens: 120,
-    });
-    console.log(response.choices[0].message.content);
-    res.json({ doctorResponse: response.choices[0].message.content.replace(/\n/g, "\\n") });
+    //const response = await openai.chat.completions.create({
+    //  model: 'gpt-3.5-turbo',
+    //  messages: [{ "role": "user", "content": userPrompt }],
+    //  max_tokens: 120,
+    //});
+    console.log(exampleResponse.choices[0].message.content);
+    res.json({ doctorResponse: exampleResponse.choices[0].message.content.replace(/\n/g, "\\n") });
   });
   
 
