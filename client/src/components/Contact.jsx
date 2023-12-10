0cde34ea319003b82d328fe1b334d5df4fd7ecae
iatsrangalov20 <iatsrangalov20@codingburgas.bg>
2023-12-10 02:59:25 +0200

    Update contact form colors

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -7,7 +7,7 @@ const Contact = () => {
 
             <body className = "antialised bg-gray-100">
                 <div className = "flex w-full min-h-screen justify-center items-center">
-                    <div className = "flex flex-col md:flex-row md:space-x-6 space-y-6 md:space-y-0 bg-cyan-700 w-full max-w-4xl p-8 sm:p-12 rounded-xl shadow-lg text-white overflow-hidden">
+                    <div className = "flex flex-col md:flex-row md:space-x-6 space-y-6 md:space-y-0 bg-indigo-500 w-full max-w-4xl p-8 sm:p-12 rounded-xl shadow-lg text-white overflow-hidden">
 
                         <div className="flex flex-col space-y-8 justify-between">
                             <div>
@@ -36,27 +36,27 @@ const Contact = () => {
                             </div>
                         </div>
                         <div className="relative">
-                            <div className="absolute w-40 h-40 bg-teal-400 rounded-full -right-28 -top-28">
+                            <div className="absolute w-40 h-40 bg-pink rounded-full -right-28 -top-28">
 
                             </div>
-                            <div className="absolute w-40 h-40 bg-teal-400 rounded-full -left-28 -bottom-24">
+                            <div className="absolute w-40 h-40 bg-pink rounded-full -left-28 -bottom-24">
 
                             </div>
                             <div className="relative z-0 bg-white rounded-xl shadow-lg p-8 text-gray-600 md:w-80">
                                 <form action="" className="flex flex-col space-y-4">
                                     <div>
                                         <label for="" className="text-sm">Your name</label>
-                                        <input type="text" placeholder="Your name" className="ring-1 ring-gray-300 w-full rounded-md px-4 py-2 mt-2 outline-none focus:ring-2 focus:ring-teal-300"/>
+                                        <input type="text" placeholder="Your name" className="ring-1 ring-gray-300 w-full rounded-md px-4 py-2 mt-2 outline-none focus:ring-2 focus:ring-pink"/>
                                     </div>
                                     <div>
                                         <label for="" className="text-sm">Email Address</label>
-                                        <input type="email" placeholder="Email Address" className="ring-1 ring-gray-300 w-full rounded-md px-4 py-2 mt-2 outline-none focus:ring-2 focus:ring-teal-300"/>
+                                        <input type="email" placeholder="Email Address" className="ring-1 ring-gray-300 w-full rounded-md px-4 py-2 mt-2 outline-none focus:ring-2 focus:ring-pink"/>
                                     </div>
                                     <div>
                                         <label for="" className="text-sm">Message</label>
-                                        <textarea placeholder="Message" rows = "4" className="ring-1 ring-gray-300 w-full rounded-md px-4 py-2 mt-2 outline-none focus:ring-2 focus:ring-teal-300"/>
+                                        <textarea placeholder="Message" rows = "4" className="ring-1 ring-gray-300 w-full rounded-md px-4 py-2 mt-2 outline-none focus:ring-2 focus:ring-pink"/>
                                     </div>
-                                    <button className="inline-block self-end bg-cyan-700 text-white font-bold rounded-lg px-6 py-2 uppercase text-sm">Send Message</button>
+                                    <button className="inline-block self-end bg-indigo-500 hover:bg-indigo-700 text-white font-bold rounded-lg px-6 py-2 uppercase text-sm">Send Message</button>
                                 </form>
                             </div>
                         </div>
@@ -68,3 +68,4 @@ const Contact = () => {
 }
 
 export default Contact;
+
